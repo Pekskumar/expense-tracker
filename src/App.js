9f84ac7b8bd934b6f4a59f03a624c08f9bc70e52
Pekskumar <prakash.montu_92@live.com>
2024-11-13 22:59:14 +0530

    app code as it is

--- src/App.js
+++ src/App.js
@@ -1,87 +1,7 @@
-// import {
-//   Navigate,
-//   Route,
-//   BrowserRouter as Router,
-//   Routes,
-// } from "react-router-dom";
-// import { Suspense } from "react";
-// import { Toaster } from 'react-hot-toast';
-// import { useSelector } from "react-redux";
-// import Home from "./Pages/AllPages/Home";
-// import Layout from "./Pages/AllPages/Layout";
-// import Todos from "./Pages/AllPages/Todos";
-// import Transations from "./Pages/AllPages/Transations";
-// import Users from "./Pages/AllPages/Users";
-// import AuthLayout from "./Pages/AuthPages/AuthLayout";
-// import ForgotPassword from "./Pages/AuthPages/ForgotPassword";
-// import ResetPassword from "./Pages/AuthPages/ResetPassword";
-
-// function App() {
-//   const Token = useSelector((state) => state.userinfo.Token);
-//   const is_logged = Token !== null ? true : false;
-//   const AllRoutes = () => (
-//     <Routes>
-//       <Route path="*" element={<Navigate to="/" />} />
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<Navigate to="/home" replace />} />
-//         <Route
-//           path="/home"
-//           element={
-//             <Suspense fallback={"Loader..."}>
-//               <Home />
-//             </Suspense>
-//           }
-//         />
-//         <Route
-//           path="/transactions"
-//           element={
-//             <Suspense fallback={"Loader..."}>
-//               <Transations />
-//             </Suspense>
-//           }
-//         />
-
-//         <Route
-//           path="/users"
-//           element={
-//             <Suspense fallback={"Loader..."}>
-//               <Users />
-//             </Suspense>
-//           }
-//         />
-//         <Route
-//           path="/todos"
-//           element={
-//             <Suspense fallback={"Loader..."}>
-//               <Todos />
-//             </Suspense>
-//           }
-//         />
-//       </Route>
-
-//     </Routes>
-//   );
-//   const AuthRoutes = () => (
-//     <Routes>
-//       <Route path="*" element={<Navigate to="/" />} />
-//       <Route path="/" element={<AuthLayout />} />
-//       <Route path="/reset-password" element={<ResetPassword />} />
-//       <Route path="/forgot-password" element={<ForgotPassword />} />
-//     </Routes>
-//   );
-//   return (
-//     <>
-//       <Toaster position="bottom-center" />
-//       <Router basename={process.env.PUBLIC_URL}>{is_logged ? <AllRoutes /> : <AuthRoutes />}</Router>
-//     </>
-//   );
-// }
-
-// export default App;
 import {
   Navigate,
   Route,
-  HashRouter as Router, // Change to HashRouter
+  BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
 import toast, { Toaster } from 'react-hot-toast';
@@ -141,7 +61,6 @@ function App() {
       </Route>
     </Routes>
   );
-
   const AuthRoutes = () => (
     <Routes>
       <Route path="*" element={<Navigate to="/" />} />
@@ -150,14 +69,12 @@ function App() {
       <Route path="/forgot-password" element={<ForgotPassword />} />
     </Routes>
   );
-
   return (
     <>
       <Toaster position="bottom-center" />
-      <Router>{is_logged ? <AllRoutes /> : <AuthRoutes />}</Router>
+      <Router basename={process.env.PUBLIC_URL}>{is_logged ? <AllRoutes /> : <AuthRoutes />}</Router>
     </>
   );
 }
 
 export default App;
-
